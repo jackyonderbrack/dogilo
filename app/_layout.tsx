699b1b38dca1c1813f2e8c198c969dc799c1c037
jackyonderbrack <mike.projektowanie@gmail.com>
2024-05-03 16:02:01 +0200

    Added clerk

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,10 +4,34 @@ import { Stack, useRouter } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 import { TouchableOpacity } from "react-native";
+import * as SecureStore from 'expo-secure-store'
+import { ClerkProvider, useAuth } from "@clerk/clerk-expo";
+
+const EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
+
+const tokenCache = {
+  async getToken(key: string) {
+    try {
+      return SecureStore.getItemAsync(key)
+    } catch (error) {
+      return null
+    }
+  },
+  async saveToken(key: string, value: string) {
+    try {
+      return SecureStore.setItemAsync(key, value)
+    } catch (error) {
+      return console.error('Wystąpił błąd podczas zapisywania wartości:', error);
+    }
+  }
+}
+
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
 } from "expo-router";
+
+
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
@@ -45,12 +69,24 @@ export default function RootLayout() {
     return null;
   }
 
-  return <RootLayoutNav />;
+  return (
+    <ClerkProvider publishableKey={EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!} tokenCache={tokenCache}>
+      <RootLayoutNav />;
+    </ClerkProvider>
+    )
 }
 
 function RootLayoutNav() {
   const HeaderLeftCloseButton = () => {
     const router = useRouter();
+    const { isLoaded, isSignedIn } = useAuth();
+
+    useEffect(() => {
+      if (!isLoaded && !isSignedIn) {
+        router.push('/(modals)/login')
+      }
+    }, [isLoaded])
+
     return (
       <TouchableOpacity onPress={() => router.back()}>
         <MaterialCommunityIcons
@@ -85,10 +121,10 @@ function RootLayoutNav() {
       <Stack.Screen
         name="(modals)/finder"
         options={{
-          headerTitle: "Wyszukiwanie",
-          headerLeft: () => <HeaderLeftCloseButton />,
+          headerTitle: "Wyszukiwanie", 
           presentation: "transparentModal",
           animation: "fade",
+          headerLeft: () => <HeaderLeftCloseButton />,
         }}
       />
     </Stack>
