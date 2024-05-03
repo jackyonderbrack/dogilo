cd567170543902a55d7d985db1fddf58a9919518
jackyonderbrack <mike.projektowanie@gmail.com>
2024-05-03 17:05:11 +0200

    Customize Login view

--- app/(modals)/login.tsx
+++ app/(modals)/login.tsx
@@ -1,12 +1,59 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from "react-native";
 import React from "react";
+import { useWarmUpBrowser } from "@/hooks/useWarmUpBrowser";
+import { defaultStyles } from "@/constants/Styles";
+import Colors from "@/constants/Colors";
 
 const login = () => {
+  useWarmUpBrowser();
+
   return (
-    <View>
-      <Text>login</Text>
+    <View style={styles.container}>
+      <TextInput
+        autoCapitalize="none"
+        placeholder="Wpisz swój e-mail"
+        style={[defaultStyles.inputField, { marginBottom: 30 }]}
+      />
+      <TouchableOpacity style={defaultStyles.btn}>
+        <Text style={defaultStyles.btnText}>Dalej</Text>
+      </TouchableOpacity>
+      <View style={styles.separatorView}>
+        <View
+          style={{
+            flex: 1,
+            borderBottomColor: "#000",
+            borderBottomWidth: StyleSheet.hairlineWidth,
+          }}
+        />
+        <Text>lub</Text>
+        <View
+          style={{
+            flex: 1,
+            borderBottomColor: "#000",
+            borderBottomWidth: StyleSheet.hairlineWidth,
+          }}
+        />
+      </View>
     </View>
   );
 };
 
 export default login;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#ebe6f7",
+    padding: 20,
+  },
+  separatorView: {
+    flexDirection: "row",
+    gap: 10,
+    alignItems: "center",
+    marginVertical: 30,
+  },
+  divider: {
+    fontFamily: "lex-bold",
+    color: Colors.grey,
+  },
+});
